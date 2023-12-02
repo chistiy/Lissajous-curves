737d1d3007e504853b13b82b774f7216100b6e65
chistiy <pechenyha11@gmail.com>
2023-12-02 14:05:50 +0300

    feat(main): finish

---
+++ go.mod
@@ -0,0 +1,4 @@
+module learningGo
+
+go 1.21.4
+
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			cycles := r.URL.Query().Get("cycles")
+			if cycles == "" {
+				cycles = "20" // значение по умолчанию
+			}
+			cyclesInt, err := strconv.ParseInt(cycles, 10, 64)
+			if err != nil {
+				http.Error(w, "Invalid value for 'cycles': "+err.Error(), http.StatusBadRequest)
+				return
+			}
+			lissajous(w, cyclesInt)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	lissajous(os.Stdout, 20)
+}
+
+func lissajous(out io.Writer, cycles int64) {
+	const (
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
